77dc1c33c8ded6ae393211cf916409dbf35f10fb
Aske <asylbek_2.06@mail.ru>
2023-08-03 14:34:19 +0600

    Initial commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+$clid = 'ak220315';
+$apikey = 'CVrUftwfwoEYOiCEFWwNHaVabjqPWhARDYmQzFb';
+$class_str = 'express';
+
+$fromLonLat = '76.93361121975246,43.230385838927816';
+$toLonLat = '76.95132452808683,43.24996393218453';
+
+$url = 'https://taxi-routeinfo.taxi.yandex.net/taxi_info';
+
+$options = array(
+    'clid' => $clid,
+    'apikey' => $apikey,
+    'rll' => $fromLonLat.'~'.$toLonLat,
+    'class_str' => $class_str,
+);
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_URL, $url.'?'.http_build_query($options));
+
+$responce = curl_exec($ch);
+curl_close($ch);
+
+echo '<pre>';
+print_r($responce);
+echo '</pre>';
